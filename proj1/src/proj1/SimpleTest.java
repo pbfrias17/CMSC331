8b93af4f64b1ec098f9ceb4b1df2c1e5cd1d4bd4
pbfrias17 <pbfrias17@gmail.com>
2015-03-16 12:38:55 -0400

    idc

---
+++ proj1/src/proj1/SimpleTest.java
@@ -0,0 +1,14 @@
+package proj1;
+
+//  Boilerplate
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class SimpleTest
+{
+	@Test
+	public void trivialTest()
+	{
+		//  Do nothing, and therefore succeed.
+	}
+}
